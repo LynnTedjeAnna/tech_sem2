c77ec28dbae18b2f625d2c341d611ce47ff00c93
Lynn Meindertsma <lmeindertsma@hotmail.nl>
2024-06-10 15:28:40 +0200

    progress assignment
     watch, next four sections done

--- watch/shared/watch_registers.c
+++ watch/shared/watch_registers.c
@@ -26,30 +26,49 @@ void watch_registers_get_config_settings(uint8_t config, bool* is_paused, time_f
 }
 
 void watch_registers_set_time_hours(uint8_t* time_bits_low, uint8_t* time_bits_high, uint8_t hours){
-
+    if (time_bits_low == NULL || time_bits_high == NULL){ return;}
+    if (hours > 11){ return;}
+    *time_bits_high = hours << 4;
 }
 
 void watch_registers_set_time_minutes(uint8_t* time_bits_low, uint8_t* time_bits_high, uint8_t minutes){
+    if (time_bits_low == NULL || time_bits_high == NULL){ return;}
+    if (minutes > 59){ return;}
 
+    *time_bits_low &= ~(0b11 << 6);
+    *time_bits_low |= (minutes << 6);
+
+    *time_bits_high &= ~(0b1111);
+    *time_bits_high |= (minutes >> 2);
 }
 
 void watch_registers_set_time_seconds(uint8_t* time_bits_low, uint8_t* time_bits_high, uint8_t seconds){
-
+    if (time_bits_low == NULL || time_bits_high == NULL){ return;}
+    if (seconds > 59){ return;}
+    *time_bits_low &= ~(0b1111);
+    *time_bits_low |= seconds;
 }
 
 void watch_registers_get_time(uint8_t time_bits_low, uint8_t time_bits_high, uint8_t* hours, uint8_t* minutes, uint8_t* seconds){
-
+    if (hours == NULL || minutes == NULL || seconds == NULL){ return;}
+    *hours = time_bits_high >> 4;
+    *minutes = time_bits_low >> 6;
+    *minutes |= time_bits_high << 2;
+    *seconds = time_bits_low & ~(0b11 << 6);
 }
 
 void watch_registers_set_date_year(uint8_t* date_bits_low, uint8_t* date_bits_high, uint8_t year){
-
+    if (date_bits_low == NULL || date_bits_high == NULL){ return;}
+    
 }
 
 void watch_registers_set_date_month(uint8_t* date_bits_low, uint8_t* date_bits_high, uint8_t month){
+    if (date_bits_low == NULL || date_bits_high == NULL){ return;}
 
 }
 
 void watch_registers_set_date_day_of_month(uint8_t* date_bits_low, uint8_t* date_bits_high, uint8_t day_of_month){
+    if (date_bits_low == NULL || date_bits_high == NULL){ return;}
 
 }
 
