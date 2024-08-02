02ef973eb6a97b1fff2559019fa6dead17c6f448
Lynn Meindertsma <lmeindertsma@hotmail.nl>
2024-08-02 17:40:09 +0200

    Revert "added files"
    This reverts commit 41fcda893e1e448d5691d6d506b33d3f28f05eb3.

--- sudoku/include/board.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Lynn Meindertsma on 02/08/2024.
-//
-
-#ifndef SUDOKU_BOARD_H
-#define SUDOKU_BOARD_H
-
-#endif //SUDOKU_BOARD_H
--- sudoku/src/board.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Lynn Meindertsma on 02/08/2024.
-//
-
-#include "../include/board.h"
